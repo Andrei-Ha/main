298b500a81d7171441e1ad4ea54d962c21d26b57
SandroShera <sherashera777@gmail.com>
2022-02-01 19:49:58 +0400

    Init.

---
+++ src/Domain/Attribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exadel.OfficeBooking.Domain
+{
+    public class Attribute :BaseModel
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ src/Domain/Office.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exadel.OfficeBooking.Domain
+{
+    class Office : BaseModel
+    {
+        public int CountryId { get; set; }
+
+        public int Floors { get; set; }
+
+    }
+}
