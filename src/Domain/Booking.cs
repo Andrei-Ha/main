fea7264cb1e97334664a7af09ab3c17fab0477a3
Anvarrich <majidovanvar30.11.2002@gmail.com>
2022-02-03 17:31:56 +0500

    update booking model
    add user, workspace and frequency

--- src/Domain/Booking.cs
+++ src/Domain/Booking.cs
@@ -1,14 +1,18 @@
-﻿namespace Exadel.OfficeBooking.Domain;
+﻿using Exadel.OfficeBooking.Domain.OfficePlan;
+
+namespace Exadel.OfficeBooking.Domain;
 
 public class Booking
 {
-    public int Id { get; set; }
-    //name of the workspace that user wants to book
-    public string WorkspaceName { get; set; }
-    //booking of office must be approved by Admin, otherwise by default it is false
-    public bool IsApproved { get; set; } = false;
-    //WaitingList is used to store users who want to book this Workspace
-    //public List<User> WaitingList { get; set; }
+    public Guid Id { get; set; }
+    public WorkSpace WorkSpace { get; set; } = new();
+    public User User { get; set; } = new();
+    
+    public string RecurringDays { get; set; } = string.Empty;
+    public bool IsRecurringMonthly { get; set; }
+    public bool IsRecurringYearly { get; set; }
+    
+    public bool IsApproved { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
 }
