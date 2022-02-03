70d7f8e6ef6d68d7f3dc56fecb6b88eeb64f1c6e
Anvarrich <majidovanvar30.11.2002@gmail.com>
2022-02-03 18:58:40 +0500

    temporary update

--- src/Domain/Booking.cs
+++ src/Domain/Booking.cs
@@ -1,12 +1,12 @@
-﻿using Exadel.OfficeBooking.Domain.OfficePlan;
+﻿//using Exadel.OfficeBooking.Domain.OfficePlan;
 
 namespace Exadel.OfficeBooking.Domain;
 
 public class Booking
 {
     public Guid Id { get; set; }
-    public WorkSpace WorkSpace { get; set; } = new();
-    public User User { get; set; } = new();
+    //public WorkSpace WorkSpace { get; set; } = new();
+    //public User User { get; set; } = new();
     
     public string RecurringDays { get; set; } = string.Empty;
     public bool IsRecurringMonthly { get; set; }
