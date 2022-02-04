5023dc666866af2340ba33a2e68ec10a3e71e6f9
Василий Василий <vas666@yandex.by>
2022-02-04 13:51:24 +0300

    Commit2

--- src/Domain/Booking.cs
+++ src/Domain/Booking.cs
@@ -3,31 +3,9 @@ using Exadel.OfficeBooking.Domain.Person;
 
 namespace Exadel.OfficeBooking.Domain
 {
-    //Predefined microsoft enum
-    //https://docs.microsoft.com/en-us/dotnet/api/system.dayofweek?view=net-6.0
-    //
-    //public enum DayOfWeek
-    //{
-    //    Sunday = 0,
-    //    Monday = 1,
-    //    Tuesday = 2,
-    //    Wednesday = 3,
-    //    Thursday = 4,
-    //    Friday = 5,
-    //    Saturday = 6
-    //}
-
     public class Booking : BaseModel
     {
         public string RecurringDays { get; set; } = string.Empty;
-
-        public bool[] WeeklyPattern = new bool[7];
-
-        public bool[] MonthlyPattern = new bool[31];
-
-        public bool IsRecurringWeekly { get; set; }
-
-        public bool IsRecurringMonthly { get; set; }
 
         public DateTime StartDate { get; set; }
 
