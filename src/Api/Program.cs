58403a3adb58fda5de198316b7d84a1befcbe868
Андрей Сергеич <asgoreglyad@gmail.com>
2022-02-06 15:46:44 +0300

    PR from Local

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -4,7 +4,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite("Data Source = DB\\OfficeBooking.db"));
+string connection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connection));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
