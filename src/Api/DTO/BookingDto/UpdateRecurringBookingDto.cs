aa96f1f347b3b8ee683a4503ce8bf179e7459921
Anvarrich <majidovanvar30.11.2002@gmail.com>
2022-02-14 20:57:26 +0500

    Add booking dto files

---
+++ src/Api/DTO/BookingDto/UpdateRecurringBookingDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Exadel.OfficeBooking.Domain.Bookings;
+
+namespace Exadel.OfficeBooking.Api.DTO.BookingDto;
+
+public class UpdateRecurringBookingDto
+{
+    public Guid Id { get; set; }
+    public Guid WorkplaceId { get; set; }
+
+    public DateTime StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
+
+    public int? Count { get; set; }
+    public int Interval { get; set; } = 1;
+    public string RecurringWeekDays { get; set; } = "0000000";
+    public RecurringFrequency Frequency { get; set; }
+}
