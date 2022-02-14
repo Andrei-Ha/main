d980f3dde16305b48d08c9a2781a740bf33c9bd8
Anvarrich <majidovanvar30.11.2002@gmail.com>
2022-02-14 20:57:49 +0500

    Add booking interface

---
+++ src/Api/Interfaces/IBookingService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Exadel.OfficeBooking.Api.DTO;
+using Exadel.OfficeBooking.Api.DTO.BookingDto;
+
+namespace Exadel.OfficeBooking.Api.Interfaces;
+
+public interface IBookingService
+{
+    Task<ServiceResponse<GetBookingDto[]>> GetAllBookings();
+    Task<ServiceResponse<GetBookingDto>> GetBookingById(Guid id);
+
+    Task<ServiceResponse<GetOneDayBookingDto>> CreateBooking(AddBookingDto bookingDto);
+    Task<ServiceResponse<GetOneDayBookingDto>> UpdateBooking(UpdateBookingDto bookingDto);
+
+    Task<ServiceResponse<GetRecurringBookingDto>> CreateRecurringBooking(AddRecurringBookingDto bookingDto);
+    Task<ServiceResponse<GetBookingDto>> UpdateRecurringBooking(UpdateRecurringBookingDto bookingDto);
+
+    Task<ServiceResponse<GetBookingDto[]>> DeleteBooking(Guid id);
+    
+}
