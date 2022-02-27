485cb36e4afb31aa8938f0b36fee89deeca6c391
Василий Василий <vas666@yandex.by>
2022-02-27 04:49:14 +0300

    Commit9

--- src/TelegramApi/EF/UserState.cs
+++ src/TelegramApi/EF/UserState.cs
@@ -16,20 +16,6 @@ namespace Exadel.OfficeBooking.TelegramApi.EF
 
         public Guid? UserId { get; set; }
 
-        public string Token { get; set; } = string.Empty;
-
-        public string City { get; set; } = string.Empty;
-
-        public Guid? OfficeId { get; set; }
-
-        public Guid? MapId { get; set; }
-
-        public Guid? Workplace { get; set; }
-
-        public DateTime? BookDate { get; set; }
-
-        public bool IsSuccess { get; set; } = false;
-
         public List<Propositions>? Propositions { get; set; }
     }
 }
