f3658aa02f3fc61d629dab6b4e7f15ff452ad77c
Anvarrich <majidovanvar30.11.2002@gmail.com>
2022-03-01 01:50:09 +0500

    Update state machine.(We will create new table in telegramApi so modify method SetNextStep())

--- src/TelegramApi/Controllers/TelegramApiController.cs
+++ src/TelegramApi/Controllers/TelegramApiController.cs
@@ -1,10 +1,21 @@
+using System;
+using System.IO;
 using Exadel.OfficeBooking.TelegramApi.FSM;
 using Microsoft.AspNetCore.Mvc;
 using System.Net.Http;
+using System.Net.Http.Json;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using Exadel.OfficeBooking.TelegramApi.DTO;
+using Exadel.OfficeBooking.TelegramApi.DTO.PersonDto;
+using Mapster;
+using Newtonsoft.Json;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
+using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace Exadel.OfficeBooking.TelegramApi.Controllers
 {
@@ -12,16 +23,22 @@ namespace Exadel.OfficeBooking.TelegramApi.Controllers
     [Route("api/message/update")]
     public class TelegramApiController : ControllerBase
     {
-        private readonly TelegramBotClient _telegramBotClient;
-        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly TelegramBotClient _client;
+        private readonly HttpClient _http;
 
         private readonly StateMachine _stateMachine;
 
-        public TelegramApiController(TelegramBot telegramBot, IHttpClientFactory httpClientFactory, StateMachine stateMachine)
+        public TelegramApiController(TelegramBot telegramBot, StateMachine stateMachine, HttpClient http)
         {
-            _telegramBotClient = telegramBot.GetBot().Result;
-            _httpClientFactory = httpClientFactory;
+            _client = telegramBot.GetBot().Result;
             _stateMachine = stateMachine;
+            _http = http;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok("Get request working");
         }
 
         [HttpPost]
@@ -30,19 +47,43 @@ namespace Exadel.OfficeBooking.TelegramApi.Controllers
             if (update.Message!.Type != MessageType.Text)
                 return Ok();
 
-
-            var chatId = update.Message.Chat.Id;
-            var messageText = update.Message.Text;
-
             await _stateMachine.IncomingUpdateHandle(update);
-
-            messageText = messageText ?? "no text";
-            await _telegramBotClient.SendTextMessageAsync(
-                chatId: chatId,
-                text: messageText,
-                parseMode: ParseMode.Markdown
-                );
             return Ok();
+        }
+        
+        public async Task SetNextStep(long userTelegramId, string nextStepName)
+        {
+            //find user with given telegramId
+            string getEndpoint = $"user/telegram/{userTelegramId}";
+            ServiceResponse<GetUserDto>? user = await GetRequestServer<ServiceResponse<GetUserDto>>(getEndpoint);
+            
+            //change user's state
+            string putEndpoint = $"user/{user.Data.Id}";
+            SetUserDto updatedUser = user.Data.Adapt<SetUserDto>();
+            updatedUser.StepName = nextStepName;
+            
+            await PutRequestServer<GetUserDto, SetUserDto>(putEndpoint, updatedUser);
+        }
+        public async Task<T1?> PutRequestServer<T1, T2>(string endpoint, T2 putObj)
+        {
+            string uri = $"https://localhost:7110/api/{endpoint}";
+            var response = await _http.PutAsJsonAsync(uri, putObj);
+
+            Stream responseStream = await response.Content.ReadAsStreamAsync();
+            var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+            T1? responseObject = await JsonSerializer.DeserializeAsync<T1>(responseStream, options);
+            
+            return responseObject;
+        }
+        public async Task<T?> GetRequestServer<T>(string endpoint)
+        {
+            string uri = $"https://localhost:7110/api/{endpoint}";
+            HttpResponseMessage response = await _http.GetAsync(uri);
+            Stream responseStream = await response.Content.ReadAsStreamAsync();
+        
+            var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+            T? responseObject = await JsonSerializer.DeserializeAsync<T>(responseStream, options);
+            return responseObject;
         }
     }
 }
