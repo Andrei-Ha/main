383c76fa725545b630131c3f5e059c5e14fa416b
Anvarxon <55556062+Anvarxon@users.noreply.github.com>
2022-03-11 00:39:03 +0500

    7th file is reviewed

--- src/TelegramApi/Steps/SpecParamChoice.cs
+++ src/TelegramApi/Steps/SpecParamChoice.cs
@@ -10,6 +10,10 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Telegram.Bot.Types;
 
+/*
+At this step, user can specify floor and/or choose the exact place at office for booking.
+*/
+
 namespace Exadel.OfficeBooking.TelegramApi.Steps
 {
     public class SpecParamChoice : StateMachineStep
@@ -29,7 +33,7 @@ namespace Exadel.OfficeBooking.TelegramApi.Steps
                 return _state;
             }
 
-            // Yes, I have special preferences
+            // If the choice is "Yes, I have special preferences"
             if (text == _state.Propositions[0])
             {
                 _state.IsSpecifyWorkplace = true;
@@ -37,7 +41,7 @@ namespace Exadel.OfficeBooking.TelegramApi.Steps
                 _state.Propositions = new() { "yes", "no" };
                 _state.NextStep = nameof(FloorChoice);
             }
-            // No, I can take any available workplace
+            // If the choice is "No, I can take any available workplace"
             else if (text == _state.Propositions[1])
             {
                 _state.IsSpecifyWorkplace = false;
