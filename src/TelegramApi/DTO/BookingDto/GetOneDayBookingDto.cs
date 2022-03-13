688a7cee5c3a19707828e7a93712332664d27370
Anvarrich <majidovanvar30.11.2002@gmail.com>
2022-03-13 15:13:48 +0500

    add booking Dtos

---
+++ src/TelegramApi/DTO/BookingDto/GetOneDayBookingDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Exadel.OfficeBooking.TelegramApi.DTO.BookingDto;
+
+public class GetOneDayBookingDto
+{
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+    public Guid WorkplaceId { get; set; }
+    public Guid? ParkingPlaceId { get; set; }
+
+    public DateTime Date { get; set; }
+}
---
+++ src/TelegramApi/DTO/BookingDto/GetRecurringBookingDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exadel.OfficeBooking.TelegramApi.DTO.BookingDto;
+
+public class GetRecurringBookingDto
+{
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+    public Guid WorkplaceId { get; set; }
+    public Guid? ParkingPlaceId { get; set; }
+
+    public DateTime StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
+
+    public int? Count { get; set; }
+    public int Interval { get; set; } = 1;
+    public WeekDays RecurringWeekDays { get; set; }
+    public RecurringFrequency Frequency { get; set; }
+}
